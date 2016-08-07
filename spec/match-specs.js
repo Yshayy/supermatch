ce97281471f3af2bfc75cf55149e8b3b52e29937
yshayy <yshayy@gmail.com>
2016-08-08 01:01:22 +0300

    add more string cases"

--- spec/match-specs.js
+++ spec/match-specs.js
@@ -5,10 +5,22 @@ import match from '../src/match'
 describe('match specs', function(){
   describe('matching a string', ()=>{
     it('should support fixed strings', ()=>{
-      const result = match('abc', [[
+      const result = match('abc', [
         'abc', ()=> true
-      ]])
+      ])
       expect(result).to.be.eql(true)
+    })
+    it('should support regex', ()=>{
+      const result = match('abc', [
+        /ab/, ()=> true
+      ])
+      expect(result).to.be.eql(true)
+    })
+    it('should capture regex value', ()=>{
+      const result = match('abc', [
+        /ab/, (m)=> m
+      ])
+      expect(result[0]).to.be.eql('ab')
     })
   })
 })
--- src/match.js
+++ src/match.js
@@ -3,21 +3,26 @@ function buildPattern(pattern){
     if (typeof(pattern) ==='string'){
       return v === pattern
     }
+    if (pattern instanceof RegExp){
+      return v.match(pattern)
+    }
   }
 }
 
+function pairs(arr){
+  return arr.reduce((acc, next, i)=> i%2 ==0 ? [...acc, [next]] : [...acc.slice(1), [...acc[acc.length-1], next]], [])
+}
+
 function createMatcher(exp){
-  const patternFunctions = exp.map(([pattern, action])=>([buildPattern(pattern), action]))
+  const patternFunctions = pairs(exp).map(([pattern, action])=>([buildPattern(pattern), action]))
   return (value)=>{
     for (let [fn, action] of patternFunctions){
-      if (fn(value)){
-        return action()
-      }
+      let result = fn(value)
+      if (result) return action(result)
     }
   }
 }
 
 export default function match(match, exp){
-  console.log(match, exp);
   return createMatcher(exp)(match)
 }
