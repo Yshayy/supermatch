671fb3702c9d3ea953f1606241198f8689c7e520
yshayy <yshayy@gmail.com>
2016-11-11 22:18:31 +0200

    tweak tests a bit

--- __tests__/match-specs.js
+++ __tests__/match-specs.js
@@ -39,26 +39,32 @@ describe('match specs', function(){
   })
   describe('matching an object', ()=>{
     it('should match object structure', ()=>{
-      const result = match({'a': 'b'}, 
-        {'a': 'b'}, ()=> true
+      const result = match({a: 'b'}, 
+        {a: 'b'}, ()=> true
       )
       expect(result).toBe(true)
     })
     it('should match complex object structure', ()=>{
-      const result = match({'a': {c:'b'}}, 
-        {'a': {c:'b'}}, ()=> true
+      const result = match({a: {c:'b'}}, 
+        {a: {c:'b'}}, ()=> true
       )
       expect(result).toBe(true)
     })
     it('should able to capture variable', ()=>{
-      const result = match({'a': 'b'}, 
-        {'a': $.x}, ({x})=> x === 'b'
+      const result = match({a: 'b'}, 
+        {a: $.x}, ({x})=> x === 'b'
       )
       expect(result).toBe(true)
     })
     it('should able to capture multiple variable', ()=>{
-      const result = match({'x': 'a', 'y': 'b'}, 
-        {'x': $.x, 'y': $.y}, ({x,y})=> x === 'a' && y === 'b'
+      const result = match({x: 'a', y: 'b', z:{zz:'c'}}, 
+        {
+          x: $.x, 
+          y: $.y, 
+          z: {
+            zz: $.zz
+          }
+        }, ({x, y, zz})=> x === 'a' && y === 'b' && zz === 'c'
       )
       expect(result).toBe(true)
     })
